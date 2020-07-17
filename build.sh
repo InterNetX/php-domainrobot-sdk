de8768a98abe186d365abd8891b4354980d071b0
benjamin.krammel <bk@internetx.com>
2020-07-17 13:35:12 +0200

    add internetx/internetx-swagger-files

--- build.sh
+++ build.sh
@@ -1,5 +1,7 @@
+#!/usr/bin/env sh
+
 cd src/Model
-java -jar -Dmodels -DsupportingFiles -DmodelDocs=false -DmodelTests=false ../../swagger-codegen-cli.jar generate  -l php -i ../Lib/specs.json -c ../../swagger.conf
+java -jar -Dmodels -DsupportingFiles -DmodelDocs=false -DmodelTests=false ../../swagger-codegen-cli.jar generate  -l php -i ../../vendor/internetx/internetx-swagger-files/src/domainrobot.json -c ../../swagger.conf
 mv lib/Model/*.php .
 rm -rf lib
 cd ../..

5923a8226a55cf4fae52a477e7d8369cf066ef05
benjamin.krammel <bk@internetx.com>
2020-08-18 15:12:43 +0200

    add pc.domains models

--- build.sh
+++ build.sh
@@ -2,6 +2,7 @@
 
 cd src/Model
 java -jar -Dmodels -DsupportingFiles -DmodelDocs=false -DmodelTests=false ../../swagger-codegen-cli.jar generate  -l php -i ../../vendor/internetx/internetx-swagger-files/src/domainrobot.json -c ../../swagger.conf
+java -jar -Dmodels -DsupportingFiles -DmodelDocs=false -DmodelTests=false ../../swagger-codegen-cli.jar generate  -l php -i ../../price_estimation.yaml -c ../../swagger.conf
 mv lib/Model/*.php .
 rm -rf lib
 cd ../..

18c4f10137b58cf6295b844692affe598f0844cc
Christian Pleintinger <christian.pleintinger@intranetx.com>
2020-11-27 10:55:24 +0100

    DEV-3204
    Signed-off-by: Christian Pleintinger <christian.pleintinger@intranetx.com>

---
+++ example/whois/WhoisMulti.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Example;
+
+use Domainrobot\Domainrobot;
+use Domainrobot\Lib\DomainrobotAuth;
+use Domainrobot\Lib\DomainrobotException;
+
+class SDKController
+{
+    /**
+     * Search the Whois Information
+     * of some domains
+     *
+     * @return string
+     */
+    public function domainStudioSearch()
+    {
+        $domainrobot = new Domainrobot([
+            'url' => 'https://api.autodns.com/v1',
+            'auth' => new DomainrobotAuth([
+                'user' => 'username',
+                'password' => 'password',
+                'context' => 4
+            ])
+        ]);
+
+        // Set an array of Domains to search 
+        // after the Whois Information
+        $domains = [ 'example.com', 'example.xyz' ];
+
+        try {
+            $whoisStatus = $domainrobot->whois->multi($domains);
+        } catch (DomainrobotException $exception) {
+            return $exception->getError();
+        }
+        
+        return $whoisStatus;
+    }
+}
